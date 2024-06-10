61b3d039f7fbe0a5a19fabdca69681a2b1766b7c
Suyog Poudel <suyogpoudel583@gmail.com>
2024-06-10 22:23:39 +0545

    fixed

--- script.js
+++ script.js
@@ -1,92 +1,123 @@
+"use strict";
+
+// Tasks
+class Todo {
+  #isDone = false;
+  #element;
+  id = (Date.now() + "").slice(-10);
+  constructor(task) {
+    this.task = task; // Written in input field
+  }
+
+  _setDone() {
+    this.#isDone = !this.#isDone;
+
+    if (true) return;
+  }
+}
+
 const form = document.querySelector(".main-form");
 const input = document.querySelector(".input-box");
 
 const todos = document.querySelector(".todos");
-// const todo = document.querySelectorAll(".todo");
-let todoArr = [];
 
-const renderTasks = function (task) {
-  let html = `
-  <li class="todo">
-    ${task}
-    <div class="icons">
-      <ion-icon
-        class="icon icon--done"
-        name="checkmark-circle"
-      ></ion-icon>
-      <ion-icon
-        class="icon icon--remove"
-        name="close-circle"
-      ></ion-icon>
-    </div>
-  </li>
-  `;
+class App {
+  #todoArr = [];
 
-  todos.insertAdjacentHTML("beforeend", html);
-};
+  constructor() {
+    form.addEventListener("submit", this._newTask.bind(this));
 
-const setLocalStorage = function (task) {
-  localStorage.setItem("todos", JSON.stringify(task));
-};
+    todos.addEventListener("click", this._getListClicks.bind(this));
 
-const getLocalStorage = function () {
-  const data = JSON.parse(localStorage.getItem("todos"));
-
-  // console.log(data);
-
-  if (!data) return;
-
-  todoArr = data;
-
-  todoArr.forEach((task) => {
-    renderTasks(task);
-  });
-};
-
-const reset = () => localStorage.removeItem("todos");
-
-// const getLocalStorage = function () {
-//   const data = JSON.parse(localStorage.getItem("todos"));
-//   if (!data) return;
-
-//   todoArr = data;
-
-//   todoArr.forEach((todo) => renderTasks(todo));
-// };
-
-getLocalStorage();
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const task = input.value;
-  if (!task) return;
-
-  console.log(task);
-
-  todoArr.push(task);
-
-  renderTasks(task);
-
-  input.value = "";
-
-  setLocalStorage(todoArr);
-});
-
-todos.addEventListener("click", function (e) {
-  const todo = e.target.closest(".todo");
-
-  // console.log(todo);
-
-  if (
-    !e.target.classList.contains("icon--done") &&
-    !e.target.classList.contains("icon--remove")
-  )
-    return;
-
-  if (e.target.classList.contains("icon--done")) todo.classList.toggle("done");
-
-  if (e.target.classList.contains("icon--remove")) {
-    todo.remove();
+    this._getLocalStorage();
   }
-});
+
+  _newTask(e) {
+    e.preventDefault();
+
+    const task = input.value;
+    let todo;
+    // if valid input create todo object
+    if (!task) return;
+
+    todo = new Todo(task);
+    // add new task to todo array
+    this.#todoArr.push(todo);
+
+    // render task
+    this._renderTask(todo);
+
+    // clear input
+    input.value = "";
+
+    // set local storage
+    this._setLocalStorage();
+  }
+
+  _renderTask(todo) {
+    let html = `
+    <li class="todo" data-id="${todo.id}">
+      ${todo.task}
+      <div class="icons">
+        <ion-icon
+          class="icon icon--done"
+          name="checkmark-circle"
+        ></ion-icon>
+        <ion-icon
+          class="icon icon--remove"
+          name="close-circle"
+        ></ion-icon>
+      </div>
+    </li>
+    `;
+
+    todos.insertAdjacentHTML("afterbegin", html);
+  }
+
+  _getListClicks(e) {
+    const clickedBtn = e.target;
+    const taskEl = e.target.closest(".todo");
+
+    if (!taskEl) return;
+
+    console.log(taskEl);
+
+    const localStorageSub = JSON.parse(localStorage.getItem("todos"));
+    const index = localStorageSub.findIndex(
+      (data) => data.id === taskEl.dataset.id
+    );
+
+    if (clickedBtn.classList.contains("icon--done")) {
+      taskEl.classList.toggle("done");
+    }
+
+    if (clickedBtn.classList.contains("icon--remove")) {
+      taskEl.remove();
+
+      localStorageSub.splice(index, 1);
+      this.#todoArr = localStorageSub;
+      this._setLocalStorage();
+      console.log(localStorageSub);
+    }
+  }
+
+  _removeFromLocalStorage() {}
+
+  _setLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(this.#todoArr));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("todos"));
+
+    if (!data) return;
+
+    this.#todoArr = data;
+
+    this.#todoArr.forEach((task) => {
+      this._renderTask(task);
+    });
+  }
+}
+
+const app = new App();
